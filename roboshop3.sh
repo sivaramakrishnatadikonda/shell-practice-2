e2db6d0b978769bf4a9a11dfa97c12e69781614b
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-27 11:30:59 +0530

    roboshop mongodb

--- roboshop3.sh
+++ roboshop3.sh
@@ -1,12 +1,15 @@
-AMI="ami-09c813fb71547fc4f"
-SG_ID="sg-0063ebc81c5fcfe82"
-ZONE_ID="Z066140621FS3C4YYVXA1"
-DOMIN_NAME="tadikondadevops.site"
-INSTANCES=( "mongodb" "redis" "mysql" )
+#!/bin/bash
 
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0063ebc81c5fcfe82" # replace with your SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z066140621FS3C4YYVXA1" # replace with your ZONE ID
+DOMAIN_NAME="tadikondadevops.site" # replace with your domain
+
+#for instance in ${INSTANCES[@]}
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0063ebc81c5fcfe82 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0063ebc81c5fcfe82 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
@@ -16,6 +19,7 @@ do
         RECORD_NAME="$DOMAIN_NAME"
     fi
     echo "$instance IP address: $IP"
+
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
     --change-batch '
