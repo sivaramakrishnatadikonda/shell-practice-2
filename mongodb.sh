734eb43540f71da1696154a53c5dcd7c4ebbd2d1
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-27 11:21:48 +0530

    roboshop mongodb

---
+++ mongodb.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="var/log/shell-practice-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+PACKAGE=( "mongodb" "nginx" "nodejs" "rebbitmq" )
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]
+then 
+    echo "ERROR:: please run the script in root access"
+    exit 1
+else
+    echo "your are in root access"
+fi
+VALIDATION(){
+
+if [ $! -eq 0 ]
+then 
+     echo " $2  Installation ---------  $G sucessfully $N " 
+else
+     echo "$2 Installation -------- $R Failed $N "
+     exit 1
+fi 
+
+}
+
+for package in $@
+do
+dnf list module $package
+if [ $? -eq 0 ]
+then 
+    echo "Installation is not completed pleaase compelted"
+dnf install $package -y
+VALIDATION $? "$package"
+else
+    echo "Instalation is already completed please $Y skipped $N "
+fi
+done
+cp $SCRIPT_DIR/mongo.repo//etc/yum.repos.d/mongo.repo
+
+dnf install mongodb-org -y &&>>LOG_FILE 
+VALIDATE $? "installing the mongodb" 
+
+systemctl enable mongod &&>>LOG_FILE
+VALIDATE $? "enable the mongodb" 
+
+systemctl start mongod &&>>LOG_FILE
+VALIDATE $? "start the mongodb server" 
+
+sed -i 's/127.0.0.1/0.0.0.0' /etc/mongod.conf # perminatly replace the 0.0.0.0 in config file
+
+systemctl restart mongod &&>>LOG_FILE
+VALIDATE $? "restart the mongodb server" 
---
+++ roboshop3.sh
@@ -0,0 +1,36 @@
+AMI="ami-09c813fb71547fc4f"
+SG_ID="sg-0063ebc81c5fcfe82"
+ZONE_ID="Z066140621FS3C4YYVXA1"
+DOMIN_NAME="tadikondadevops.site"
+
+for instance in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0063ebc81c5fcfe82 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$DOMAIN_NAME"
+    fi
+    echo "$instance IP address: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating or Updating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }'
+done
