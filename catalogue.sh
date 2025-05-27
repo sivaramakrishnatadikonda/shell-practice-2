aa00a138ba303c40e490e151b13a0ed93ab3b4ad
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-27 14:35:51 +0530

    roboshop mongodb

--- catalogue.sh
+++ catalogue.sh
@@ -1,88 +1,81 @@
 #!/bin/bash
-USERID=$(id -u) # user id creation
+
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-N="\e[0m" # colors
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
 
-LOGS_FOLDER="var/log/roboshop-logs" #store the log
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1) # remove the . extenstion
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # add the .log on end
-SCRPIT_DIR=$PWD
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-mkdir -p $LOGS_FOLDER  #if already direcotry created ok otherwise created
-echo "script started executing : $(date)" | tee -a $LOG_FILE #append the data in log file
-
-
-if [ $USERID != 0 ]
+# check the user has root priveleges or not
+if [ $USERID -ne 0 ]
 then
-    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE #append
-    exit 1 # filed the script it will use
-else   
-    echo "your script runing in the root access"
-
-fi
-
-VALIDATE(){
-
-    if [ $1 -eq 0 ]
-then 
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE #append
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
 else
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE #append
-        exit 1 # exit 1 indicates faild the script
+    echo "You are running with root access" | tee -a $LOG_FILE
 fi
-    
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
 }
 
-dnf module list nodejs &&>>LOG_FILE
-VALIDATE $? "Nodejs Module List"
-dnf module disable nodejs -y &&>>LOG_FILE
-VALIDATE $? "Nodejs Disable"
-dnf module enable nodejs:20 -y &&>>LOG_FILE
-VALIDATE $? "Nodejs Enable"
-dnf install nodejs -y &&>>LOG_FILE
-VALIDATE $? "Install Nodejs"
+dnf module disable nodejs -y &>>$LOG_FILE # output stored
+VALIDATE $? "Disabling default nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling nodejs:20"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing nodejs:20"
 
 id roboshop
-if [ $? != 0 ]
-then 
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &&>>LOG_FILE
-    VALIDATE $? "Roboshop System User"
-else 
-    echo "otherwise Skip"
+if [ $? -ne 0 ]
+then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating roboshop system user"
+else
+    echo -e "System user roboshop already created ... $Y SKIPPING $N"
 fi
 
+mkdir -p /app 
+VALIDATE $? "Creating app directory"
 
-mkdir -p /app
-VALIDATE $? "Create App Directory"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  &&>>LOG_FILE
-VALIDATE $? "Dowanload Catalogue Code"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading Catalogue"
 
 rm -rf /app/*
 cd /app 
-unzip /tmp/catalogue.zip &&>>LOG_FILE
-VALIDATE $? "Unzip Catalogue code"
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "unzipping catalogue"
 
-npm install &&>>LOG_FILE
-VALIDATE $? "Install Dependency"
+npm install &>>$LOG_FILE
+VALIDATE $? "Installing Dependencies"
 
-cp $SCRPIT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Copying catalogue service"
 
-systemctl daemon-reload &&>>LOG_FILE
-VALIDATE $? "Restart Server"
+systemctl daemon-reload &>>$LOG_FILE
+systemctl enable catalogue  &>>$LOG_FILE
+systemctl start catalogue
+VALIDATE $? "Starting Catalogue"
 
-systemctl enable catalogue &&>>LOG_FILE
-VALIDATE $? "Enable Catalogue Server"
-
-systemctl start catalogue  &&>>LOG_FILE
-VALIDATE $? "Start Catalogue Service"
-
-cp $SCRPIT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
-
-
-dnf install mongodb-mongosh -y &&>>LOG_FILE
-VALIDATE $? "Install client Server"
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo 
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "Installing MongoDB Client"
 
 STATUS=$(mongosh --host mongodb.tadikondadevops.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $STATUS -lt 0 ]
@@ -92,4 +85,3 @@ then
 else
     echo -e "Data is already loaded ... $Y SKIPPING $N"
 fi
-
